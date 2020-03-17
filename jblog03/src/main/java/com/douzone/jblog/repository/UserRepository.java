aa1de2eaa819111dce0bc4de4758d6a2a5bf595b
dongeun <kin911014@daum.net>
2020-03-17 17:57:28 +0900

    Create UserService, UserRepository and mybatis

---
+++ jblog03/src/main/java/com/douzone/jblog/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.douzone.jblog.repository;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.douzone.jblog.vo.UserVo;
+
+@Repository
+public class UserRepository {
+	@Autowired
+	private SqlSession sqlSession; 
+	
+	public void join(UserVo userVo) {
+		sqlSession.insert("user.join", userVo);
+	}
+}
