562a61df59adf285c7281b0db003b58a24fbe30b
dongeun <kin911014@daum.net>
2020-03-25 17:54:58 +0900

    jblog appConfig #2

---
+++ jblog04/src/main/java/com/douzone/jblog/config/WebConfig.java
@@ -0,0 +1,15 @@
+//package com.douzone.jblog.config;
+//
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Import;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//
+//
+//@Configuration
+//@EnableWebMvc
+//@ComponentScan("com.douzone.jblog.controller")
+//@Import({})
+//public class WebConfig {
+//
+//}
