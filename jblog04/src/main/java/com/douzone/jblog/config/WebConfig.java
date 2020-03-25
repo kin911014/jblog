83a9d6960e1de7a29a124299a5a8f8a6a4bce6cb
dongeun <kin911014@daum.net>
2020-03-25 20:48:36 +0900

    find bug#2

--- jblog04/src/main/java/com/douzone/jblog/config/WebConfig.java
+++ jblog04/src/main/java/com/douzone/jblog/config/WebConfig.java
@@ -5,12 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import com.douzone.jblog.config.web.FileUploadConfig;
+import com.douzone.jblog.config.web.MessageConfig;
 import com.douzone.jblog.config.web.MvcConfig;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan({"com.douzone.jblog.controller"})
-@Import({MvcConfig.class})
+@Import({MvcConfig.class, MessageConfig.class})
 public class WebConfig {
 
 }
