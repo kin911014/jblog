e618f2072935e38ea36a42bea6cb52b23dfac981
dongeun <kin911014@daum.net>
2020-03-25 19:16:51 +0900

    jblog appConfig #3
    mvc resource change

--- jblog04/src/main/java/com/douzone/jblog/config/WebConfig.java
+++ jblog04/src/main/java/com/douzone/jblog/config/WebConfig.java
@@ -1,15 +1,16 @@
-//package com.douzone.jblog.config;
-//
-//import org.springframework.context.annotation.ComponentScan;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.Import;
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//
-//
-//@Configuration
-//@EnableWebMvc
-//@ComponentScan("com.douzone.jblog.controller")
-//@Import({})
-//public class WebConfig {
-//
-//}
+package com.douzone.jblog.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import com.douzone.jblog.config.web.MvcConfig;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan({"com.douzone.jblog.controller, com.douzone.jblog.exception"})
+@Import({MvcConfig.class})
+public class WebConfig {
+
+}
