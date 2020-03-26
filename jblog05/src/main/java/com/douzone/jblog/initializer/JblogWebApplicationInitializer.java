845d4cd03392e5daa0b2bea3eb048ed0ebf9fc34
dongeun <kin911014@daum.net>
2020-03-26 17:54:04 +0900

    remove web.xml and create initializer of DispatcherServlet

--- jblog05/src/main/java/com/douzone/jblog/initializer/JblogWebApplicationInitializer.java
+++ jblog05/src/main/java/com/douzone/jblog/initializer/JblogWebApplicationInitializer.java
@@ -2,7 +2,10 @@ package com.douzone.jblog.initializer;
 
 import javax.servlet.Filter;
 
+import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.FrameworkServlet;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 import com.douzone.jblog.config.AppConfig;
@@ -35,5 +38,13 @@ public class JblogWebApplicationInitializer extends AbstractAnnotationConfigDisp
 		return new Filter[] {new CharacterEncodingFilter("UTF-8", true)};
 	}
 
-
+	@Override
+	protected FrameworkServlet createDispatcherServlet(WebApplicationContext servletAppContext) {
+		// create DispatcherServlet
+		DispatcherServlet dispatcherServlet = (DispatcherServlet)super.createDispatcherServlet(servletAppContext);
+		
+		// Exception Handler가 없으면 error을 던진다.
+		dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
+		return dispatcherServlet;
+	}
 }
