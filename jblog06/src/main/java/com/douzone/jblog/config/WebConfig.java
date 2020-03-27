2578742b4ee0ea6da6334efc7cb6a6f299cc37da
dongeun <kin911014@daum.net>
2020-03-27 15:51:18 +0900

    jblog06 #3
    set fileupload

--- jblog06/src/main/java/com/douzone/jblog/config/WebConfig.java
+++ jblog06/src/main/java/com/douzone/jblog/config/WebConfig.java
@@ -1,18 +1,23 @@
 package com.douzone.jblog.config;
 
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
-import com.douzone.jblog.config.web.FileUploadConfig;
-import com.douzone.jblog.config.web.MessageConfig;
-import com.douzone.jblog.config.web.MvcConfig;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
-@ComponentScan({"com.douzone.jblog.controller"})
-@Import({MvcConfig.class, MessageConfig.class, FileUploadConfig.class})
-public class WebConfig {
+@PropertySource("classpath:com/douzone/jblog/config/config.properties")
+public class WebConfig implements WebMvcConfigurer {
+	
+	@Autowired
+	private Environment env;
+	
+	// Mvc Resources(URL Magic Mapping)
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler(env.getProperty("fileupload.resourceMapping")).addResourceLocations("file:" + env.getProperty("fileupload.uploadLocation"));
+	}
 
 }
